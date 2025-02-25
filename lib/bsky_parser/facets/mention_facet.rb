0760d1e78cf74dcc9eac02cc845b33766f0878a5
Jonathan Yeong <hey@jonathanyeong.com>
2025-02-25 13:11:51 +0000

    Move call to resolve handle into a separate method
    This fixes a rubocop method length issue.

--- lib/bsky_parser/facets/mention_facet.rb
+++ lib/bsky_parser/facets/mention_facet.rb
@@ -10,7 +10,6 @@ module BskyParser
       def process
         facets = []
         # regex based on: https://atproto.com/specs/handle#handle-identifier-syntax
-        mention_pattern = /(^|\s)(@([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)/
         matches = content.to_enum(:scan, mention_pattern).map do
           match = Regexp.last_match
           start_offset = match[1]&.length || 0
@@ -24,16 +23,10 @@ module BskyParser
         matches.each do |match|
           handle = match[:handle].to_s.strip[1..] # Trim leading @
           indices = match[:indices]
-          begin
-            resp = conn.get("/xrpc/com.atproto.identity.resolveHandle", { handle: handle })
-            if resp.success?
-              handle_did = JSON.parse(resp.body)["did"]
-              facets << build_facet(indices, handle_did)
-            end
-          rescue Faraday::Error
-            # TODO: Introduce logging
-            continue
-          end
+          did = fetch_did(handle)
+          next if did.nil?
+
+          facets << build_facet(indices, did)
         end
         facets
       end
@@ -44,6 +37,18 @@ module BskyParser
         @conn ||= Faraday.new(url: BASE_URL) do |f|
           f.request :json
         end
+      end
+
+      def fetch_did(handle)
+        resp = conn.get("/xrpc/com.atproto.identity.resolveHandle", { handle: handle })
+        JSON.parse(resp.body)["did"] if resp.success?
+      rescue Faraday::Error
+        # TODO: Introduce logging
+        nil
+      end
+
+      def mention_pattern
+        /(^|\s)(@([a-zA-Z0-9]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]([a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)/
       end
 
       def build_facet(indices, handle_did)
